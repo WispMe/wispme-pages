3a220fbccb5e31ceb4e626780bc487d476dd642f
WispMe <faizfak@gmail.com>
2022-09-28 14:16:54 +0700

    under development

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+// add bootstrap css 
+import 'bootstrap/dist/css/bootstrap.css'
+import "../assets/scss/main.scss"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
